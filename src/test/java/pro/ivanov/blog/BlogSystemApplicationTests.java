10be16ff0d7722213bd87bebabd37a1bf8d7b171
Ivan Ivanov <ivancivanovbg@gmail.com>
2023-05-14 10:45:41 +0300

    init spring boot mvc application

---
+++ src/test/java/pro/ivanov/blog/BlogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.ivanov.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
