14526aac73d6c08071e7b19efba162d6356dc0aa
Ivan Ivanov <me@ivanov.pro>
2023-05-16 00:09:40 +0300

    update base config

--- src/main/java/pro/ivanov/blog/controller/ArchiveController.java
+++ src/main/java/pro/ivanov/blog/controller/ArchiveController.java
@@ -1,4 +1,43 @@
 package pro.ivanov.blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pro.ivanov.blog.entity.Article;
+import pro.ivanov.blog.repository.ArticleRepository;
+
+import java.time.YearMonth;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/archive")
 public class ArchiveController {
+    private final ArticleRepository articleRepository;
+
+    public ArchiveController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    @GetMapping("/{month}/{year}")
+    public String index(@PathVariable int month, @PathVariable int year, Model model) {
+       List<Article> articles = this.articleRepository.findAll().stream().filter(f -> f.getCreatedOn().getMonth().getValue() == month && f.getCreatedOn().getYear() == year).toList();
+
+       model.addAttribute("articles", articles);
+
+       /*
+        Map<YearMonth, Integer> group = this.articleRepository.findAll().stream()
+                .collect(Collectors.groupingBy(article -> YearMonth.from(article.getCreatedOn()),
+                        TreeMap::new,
+                        Collectors.summingInt(m -> m.getId().intValue())));
+
+
+        */
+
+        return "home/archive";
+    }
 }
