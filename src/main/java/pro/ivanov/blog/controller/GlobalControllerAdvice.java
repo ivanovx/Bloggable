4c6972b2504f2e07096dd11c62d7fd44620c1dec
Ivan Ivanov <me@ivanov.pro>
2023-05-16 17:58:30 +0300

    update aside

--- src/main/java/pro/ivanov/blog/controller/GlobalControllerAdvice.java
+++ src/main/java/pro/ivanov/blog/controller/GlobalControllerAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import pro.ivanov.blog.repository.ArticleRepository;
 
 import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -17,9 +18,11 @@ public class GlobalControllerAdvice {
     private ArticleRepository articleRepository;
 
     @ModelAttribute("calendar")
-    public  Map<YearMonth, Integer> populateCalendar() {
-        Map<YearMonth, Integer> group = this.articleRepository.findAll().stream()
-                .collect(Collectors.groupingBy(article -> YearMonth.from(article.getCreatedOn()),
+    public Map<String, Integer> populateCalendar() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+
+        Map<String, Integer> group = this.articleRepository.findAll().stream()
+                .collect(Collectors.groupingBy(article -> YearMonth.from(article.getCreatedOn()).format(formatter),
                         TreeMap::new,
                         Collectors.summingInt(m -> m.getId().intValue())));
 
